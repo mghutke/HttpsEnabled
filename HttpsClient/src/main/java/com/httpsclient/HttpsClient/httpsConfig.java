1acb914e8c0c3c4569ec0cea44ef142584120a53
monish ghutke <monishghutke@monishs-MacBook-Pro.local>
2021-10-26 02:39:19 +0530

    http client created for HTTPS API using webclient

---
+++ HttpsClient/src/main/java/com/httpsclient/HttpsClient/httpsConfig.java
@@ -0,0 +1,19 @@
+/*
+package com.httpsclient.HttpsClient;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class httpsConfig {
+
+    @Bean
+    public WebClient createWebClient(){
+
+
+
+    }
+
+}
+*/
